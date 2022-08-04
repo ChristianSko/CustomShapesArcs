4a1700ba88c6e41945ac3b13727643896f4f00cc
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-04 08:28:47 +0200

    Initial Commit

---
+++ CustomShapesArcs/CustomShapesArcsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesArcsApp.swift
+//  CustomShapesArcs
+//
+//  Created by Skorobogatow, Christian on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesArcsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
